ad19f6d0c20e3287ffaa597973bb7e678114676a
Vivian Belenky <j.vivian.belenky@outlook.com>
2026-03-23 11:30:29 -0400

    make buttons disappear during retest

--- rp/ui_debug.c
+++ rp/ui_debug.c
@@ -220,6 +220,8 @@ static void ui_debug_retest_btn_callback(lv_event_t* p_evt)
 {
     printf("Retest requested from debug menu\n");
 
+    lv_obj_add_flag(ui_debug_back_btn, LV_OBJ_FLAG_HIDDEN);
+    lv_obj_add_flag(ui_debug_retest_btn, LV_OBJ_FLAG_HIDDEN);
     lv_label_set_text(ui_debug_label, "RETESTING...\n\nLamp will turn off,\nthen board will\nreboot when done.");
     lv_obj_invalidate(ui_debug_screen);
     lv_refr_now(NULL);
